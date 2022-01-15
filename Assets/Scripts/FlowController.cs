062205da47210d5ddc17c967bc082027a4ac761e
Cheuk Ho Yun <cheukho.yun@mail.utoronto.ca>
2022-01-15 13:52:26 -0500

    start button works now. If you press ESC the pause image will pop up

--- Assets/Scripts/FlowController.cs
+++ Assets/Scripts/FlowController.cs
@@ -5,17 +5,26 @@ using UnityEngine.UI;
 
 public class FlowController : MonoBehaviour
 {
-    public bool is_paused;
+    bool is_paused;
     bool game_ended;
 
-    public GameObject image; 
+    public GameObject pause_image; 
+    public GameObject start_button;
+
+
+    public static FlowController instance;
+
+    void Awake(){
+        instance = this;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         is_paused = false; 
-        game_ended = false; // Initialized to true before game starts
-        image.SetActive(false);
+        game_ended = true; // Initialized to true before game starts
+        pause_image.SetActive(false);
+        start_button.SetActive(true);
     }
 
     // Update is called once per frame
@@ -25,17 +34,28 @@ public class FlowController : MonoBehaviour
             is_paused = !is_paused;
         }
 
-        if(game_ended){
-            // reset score and all objects to their initial placement
-        }
-        else if (is_paused){
-            Time.timeScale = 0;
-            image.SetActive(true);
+        if(!game_ended){
+            start_button.SetActive(false);
             
+            if (is_paused){
+                Time.timeScale = 0;
+                pause_image.SetActive(true);
+                
+            }
+            else{
+                Time.timeScale = 1;
+                pause_image.SetActive(false);
+            }
         }
         else{
-            Time.timeScale = 1;
-            image.SetActive(false);
+            start_button.SetActive(true);
+        }   
+    }
+
+    public void StartGame(){
+        if (game_ended){
+            ScoreManager.instance.ResetPoint();
+            game_ended = false;
         }
     }
 }

80d4095bcdc7f12b9b84839f4e0f5aa35d7e0846
Frederick <fffredericky@gmail.com>
2022-01-15 13:34:58 -0500

    Add collision script, update the movement script as well

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -2,32 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMove : MonoBehaviour
+public class playerMove : MonoBehaviour
 {
-    private float _userVerticalInput;
-    private const float ScaleMovement = 0.25f;
-    private Transform playerTransform;
+    private float _usrHorInput;
+    private float _usrRotInput;
+    private const float movementScale = 0.25f;
 
-    private float _userRotationInput;
-    private Vector3 _userRotation;
+    private Vector3 _usrRot;
+    private Transform _transform;
+    private Rigidbody _rigidbody; 
 
     // Start is called before the first frame update
     void Start()
     {
-        playerTransform = gameObject.GetComponent<Transform>();
+        _rigidbody = GetComponent<Rigidbody>(); 
+        _transform = GetComponent<Transform>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        _userVerticalInput = Input.GetAxis("Vertical");
-        Debug.Log(_userVerticalInput);
+        _usrHorInput = Input.GetAxis("Vertical");
+        _usrRotInput = Input.GetAxis("Horizontal");
+    }
 
-        _userRotationInput = Input.GetAxis("Horizontal");
-        _userRotation = playerTransform.rotation.eulerAngles;
-        _userRotation += new Vector3(0, _userRotationInput, 0);
-
-        playerTransform.position += transform.forward * _userVerticalInput;
-        playerTransform.rotation = Quaternion.Euler(_userRotation);
+    void FixedUpdate()
+    {
+        _usrRot = _transform.rotation.eulerAngles;
+        _usrRot += new Vector3(0, _usrRotInput, 0);
+        _transform.rotation = Quaternion.Euler(_usrRot);
+        _rigidbody.velocity += _transform.forward * _usrHorInput;
     }
 }

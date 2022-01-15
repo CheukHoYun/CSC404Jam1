7c948f334d81501fb42df948c2ffcdc3ad9ab143
Sirkular <sirkular.sirkle@hotmail.com>
2022-01-15 13:28:49 -0500

    Add bee movement behaviour

--- Assets/BeeBehaviourScript.cs
+++ Assets/BeeBehaviourScript.cs
@@ -4,24 +4,38 @@ using UnityEngine;
 
 public class BeeBehaviourScript : MonoBehaviour
 {
-    private Rigidbody _rigidbody;
+    // TODO: Fiddle with detection radius
+    private const float DETECTION_RADIUS = 4.0f;
+
+    // TODO: Fiddle with speed
+    private const float MOVEMENT_SPEED = 0.05f;
+    
+    // private Rigidbody _rigidbody;
+    
     private Transform _transform;
     
     // Start is called before the first frame update
     void Start()
     {
-        _rigidbody = GetComponent<Rigidbody>();
+        // _rigidbody = GetComponent<Rigidbody>();
         _transform = GetComponent<Transform>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     private void FixedUpdate()
     {
-        
+        Collider[] hitColliders = Physics.OverlapSphere(_transform.position, DETECTION_RADIUS);
+        for (int i = 0; i < hitColliders.Length; i++)
+        {
+            // TODO: Change this to a check for the PC
+            if (hitColliders[i].name.Equals("Friend Cube"))
+            {
+                GameObject entity = hitColliders[i].gameObject;
+                Vector3 direction = Vector3.Normalize(entity.transform.position - _transform.position);
+
+                _transform.rotation = Quaternion.LookRotation(direction);
+                _transform.position += direction * MOVEMENT_SPEED;
+            }
+        }
     }
+    
 }
